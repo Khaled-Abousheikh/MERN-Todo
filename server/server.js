e4062d9a6f7cd09b714108aed46c47ddf23eb9c4
Khaled-abousheikh <Khaled-abousheikh@gmail.com>
2020-10-24 20:41:51 +0300

    server update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promsie = global.Promise;
 const db = {};
 db.mongoose = mongoose;
-db.url = 'mongodb://localhost:27017/todo';
-db.models = require('./model.js');
+db.url = "mongodb://localhost:27017/todo";
+db.models = require("./model.js");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 module.exports = db;
